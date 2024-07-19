4fc935cfe265d10308711e4c4b56cfa1c5a9ce28
abdelrhmanadel200 <abdelrhmanadel2462005@gmail.com>
2024-07-19 22:58:45 +0300

    Update index.jsx

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,27 +1,39 @@
-import React from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import { useAuth } from '../../index.jsx'
-import { doSignOut } from '../../firebase/auth'
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useAuth } from '../../index.jsx';
+import { doSignOut } from '../../firebase/auth';
 
 
 const Header = () => {
-    const navigate = useNavigate() 
-    const { userLoggedIn } = useAuth()
-    return (
-        <nav className='flex flex-row gap-x-2 w-full z-20 fixed top-0 left-0 h-12 border-b place-content-center items-center bg-gray-200'>
-            {
-                userLoggedIn
-                    ?
-                    <>
-                        <button onClick={() => { doSignOut().then(() => { navigate('/login') }) }} className='text-sm text-blue-600 underline'>Logout</button>
-                    </>
-                    :
-                    <>
-     
-                    </>
-            }
-        </nav>
-    )
-}
+  const navigate = useNavigate();
+  const { userLoggedIn } = useAuth();
+  return (
+    <nav className="flex justify-between items-center py-4">
+      <div className="flex items-center">
+        {/* <h1 className="text-3xl font-bold text-gray-800">Weather App</h1> */}
+      </div>
+      <div className="flex items-center">
+        {
+          userLoggedIn
+            ?
+            <>
+              <button onClick={() => { doSignOut().then(() => { navigate('/login') }) }} className="text-sm text-black bg-gray-500 transition duration-300 ease-in-out hover:bg-black hover:text-white">
+                Logout
+              </button>
+            </>
+            :
+            <>
+              {/* <Link to="/login" className="text-sm text-gray-600 hover:text-gray-800">
+                Login
+              </Link>
+              <Link to="/register" className="text-sm text-gray-600 hover:text-gray-800 ml-2">
+                Register
+              </Link> */}
+            </>
+        }
+      </div>
+    </nav>
+  );
+};
 
-export default Header
+export default Header;
