e68f49c3f66158038e0139afa4392e91e66c394c
abdelrhmanadel200 <abdelrhmanadel2462005@gmail.com>
2024-07-19 04:06:51 +0300

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,5 +1,5 @@
-import { initializeApp } from "firebase";
-import { getAuth } from "./firebase/auth";
+import { initializeApp } from "./firebase/app";
+import { getAuth } from "firebase/auth";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
