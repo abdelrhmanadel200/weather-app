b4fa0e05ad2021d2f2c6767d0be477adaa9392d1
abdelrhmanadel200 <abdelrhmanadel2462005@gmail.com>
2024-07-19 05:03:59 +0300

    Update hom.js

--- src/components/homeee/hom.js
+++ src/components/homeee/hom.js
@@ -7,9 +7,9 @@ export default function LoginSignUp ()  {
   return (
     <div className='container' >
       
-      <h1 style={{ color: '#2196f3' width: '300'}}>
+      <h4 style={{ color: '#2196f3' width: '300'}}>
         Weather App
-      </h1>
+      </h4>
       <img className='bck' src={img} />
       <p>Please login or sign up to access the weather app</p>
       <button className='login' style={{ marginRight: 70, backgroundColor: '#2196f3', color: 'white', padding: '10px 20px', border: 'none', borderRadius: '5px' }}><Link to={"login"}>Login</Link></button>
