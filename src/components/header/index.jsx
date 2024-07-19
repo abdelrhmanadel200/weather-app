3be0bee98a8ded22c9c2461ecdff60629684ccc2
abdelrhmanadel200 <abdelrhmanadel2462005@gmail.com>
2024-07-19 03:35:21 +0300

    Update index.jsx

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link, useNavigate } from 'react-router-dom'
-import { useAuth } from '../../contexts/authContext'
+import { useAuth } from '../../index.jsx'
 import { doSignOut } from '../../firebase/auth'
 
 
