3011dc40c58d354d86fd3c27cf69a4920de5b7e0
abdelrhmanadel200 <abdelrhmanadel2462005@gmail.com>
2024-07-19 22:59:23 +0300

    Update hom.js

--- src/components/homeee/hom.js
+++ src/components/homeee/hom.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import img from './bck.jpg';
-
+import './LoginSignUp.css'
 export default function LoginSignUp ()  {
   return (
     <div className='container' >
       
-      <h1 style={{ fontSize: 36, color: '#2196f3', width: 300 }}>
+      <h1 >
         Weather App
       </h1>
-      <img className='bck' src={img} />
+      <img className='bck' src={img}/>
       <p>Please login or sign up to access the weather app</p>
       <button className='login' style={{ marginRight: 70, backgroundColor: '#2196f3', color: 'white', padding: '10px 20px', border: 'none', borderRadius: '5px' }}><Link to={"login"}>Login</Link></button>
       <button className='signup' style={{ backgroundColor: '#000000', color: 'white', padding: '10px 20px', border: 'none', borderRadius: '5px' }}><Link to={"Register"}>Sign up</Link></button>
