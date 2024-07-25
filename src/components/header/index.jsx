dfcdc3b8dc00689748a5457b29bb1ef03a8410cd
abdelrhmanadel200 <abdelrhmanadel2462005@gmail.com>
2024-07-25 03:22:20 +0300

    Update index.jsx

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -2,22 +2,23 @@ import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../../index.jsx';
 import { doSignOut } from '../../firebase/auth';
-
+import SearchBar from '../../SearchBar.js'; // Import the SearchBar component
 
 const Header = () => {
   const navigate = useNavigate();
   const { userLoggedIn } = useAuth();
   return (
-    <nav className="flex justify-between items-center py-4">
+    <nav className="justify-between items-center py-4 text-white">
       <div className="flex items-center">
-        {/* <h1 className="text-3xl font-bold text-gray-800">Weather App</h1> */}
+        <h1 className="text-3xl font-bold">Weather App</h1>
       </div>
-      <div className="flex items-center">
+      <div className="flex justify-center items-center w-full">
         {
           userLoggedIn
             ?
             <>
-              <button onClick={() => { doSignOut().then(() => { navigate('/login') }) }} className="text-sm text-black bg-gray-500 transition duration-300 ease-in-out hover:bg-black hover:text-white">
+              <SearchBar className="mx-auto" /> {/* Center the SearchBar component horizontally */}
+              <button onClick={() => { doSignOut().then(() => { navigate('/login') }) }} class="absolute top-0 right-0 text-sm bg-green-700 hover:bg-green-900 transition duration-300 ease-in-out px-4 py-2 rounded">
                 Logout
               </button>
             </>
